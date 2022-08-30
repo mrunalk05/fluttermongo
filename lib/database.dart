a4aa1c686c445f8f185e8ea244be9f7b58308ede
Mrunal Khade <83811490+mrunalk05@users.noreply.github.com>
2022-08-30 20:00:20 +0530

    Update database.dart

--- lib/database.dart
+++ lib/database.dart
@@ -1,1 +1,6 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+const MONGO_URL =
+    "mongodb+srv://Mrunal:Mrunal@cluster0.3z33w1n.mongodb.net/weather?retryWrites=true&w=majority";
+const COLLECTION = "weatherapi";
 
